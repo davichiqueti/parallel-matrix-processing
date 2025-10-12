3213376662343484d9fe5b3244458adeb379addc
Davi Chiqueti <davichiqueti@gmail.com>
2025-10-12 19:35:55 -0300

    updated script file

--- script.sh
+++ script.sh
@@ -289,3 +289,116 @@ make r-parallel rows=150 cols=100000 threads=24
 make r-threadpool rows=150 cols=100000 threads=24
 make r-parallel rows=150 cols=100000 threads=36
 make r-threadpool rows=150 cols=100000 threads=36
+
+# Adding other numbers of threads that were not runned first time
+make r-parallel rows=1 cols=100000 threads=32
+make r-threadpool rows=1 cols=100000 threads=32
+make r-parallel rows=1 cols=100000 threads=48
+make r-threadpool rows=1 cols=100000 threads=48
+make r-parallel rows=1 cols=100000 threads=64
+make r-threadpool rows=1 cols=100000 threads=64
+
+make r-parallel rows=10 cols=100000 threads=32
+make r-threadpool rows=10 cols=100000 threads=32
+make r-parallel rows=10 cols=100000 threads=48
+make r-threadpool rows=10 cols=100000 threads=48
+make r-parallel rows=10 cols=100000 threads=64
+make r-threadpool rows=10 cols=100000 threads=64
+
+make r-parallel rows=20 cols=100000 threads=32
+make r-threadpool rows=20 cols=100000 threads=32
+make r-parallel rows=20 cols=100000 threads=48
+make r-threadpool rows=20 cols=100000 threads=48
+make r-parallel rows=20 cols=100000 threads=64
+make r-threadpool rows=20 cols=100000 threads=64
+
+make r-parallel rows=30 cols=100000 threads=32
+make r-threadpool rows=30 cols=100000 threads=32
+make r-parallel rows=30 cols=100000 threads=48
+make r-threadpool rows=30 cols=100000 threads=48
+make r-parallel rows=30 cols=100000 threads=64
+make r-threadpool rows=30 cols=100000 threads=64
+
+make r-parallel rows=40 cols=100000 threads=32
+make r-threadpool rows=40 cols=100000 threads=32
+make r-parallel rows=40 cols=100000 threads=48
+make r-threadpool rows=40 cols=100000 threads=48
+make r-parallel rows=40 cols=100000 threads=64
+make r-threadpool rows=40 cols=100000 threads=64
+
+make r-parallel rows=50 cols=100000 threads=32
+make r-threadpool rows=50 cols=100000 threads=32
+make r-parallel rows=50 cols=100000 threads=48
+make r-threadpool rows=50 cols=100000 threads=48
+make r-parallel rows=50 cols=100000 threads=64
+make r-threadpool rows=50 cols=100000 threads=64
+
+make r-parallel rows=60 cols=100000 threads=32
+make r-threadpool rows=60 cols=100000 threads=32
+make r-parallel rows=60 cols=100000 threads=48
+make r-threadpool rows=60 cols=100000 threads=48
+make r-parallel rows=60 cols=100000 threads=64
+make r-threadpool rows=60 cols=100000 threads=64
+
+make r-parallel rows=70 cols=100000 threads=32
+make r-threadpool rows=70 cols=100000 threads=32
+make r-parallel rows=70 cols=100000 threads=48
+make r-threadpool rows=70 cols=100000 threads=48
+make r-parallel rows=70 cols=100000 threads=64
+make r-threadpool rows=70 cols=100000 threads=64
+
+make r-parallel rows=80 cols=100000 threads=32
+make r-threadpool rows=80 cols=100000 threads=32
+make r-parallel rows=80 cols=100000 threads=48
+make r-threadpool rows=80 cols=100000 threads=48
+make r-parallel rows=80 cols=100000 threads=64
+make r-threadpool rows=80 cols=100000 threads=64
+
+make r-parallel rows=90 cols=100000 threads=32
+make r-threadpool rows=90 cols=100000 threads=32
+make r-parallel rows=90 cols=100000 threads=48
+make r-threadpool rows=90 cols=100000 threads=48
+make r-parallel rows=90 cols=100000 threads=64
+make r-threadpool rows=90 cols=100000 threads=64
+
+make r-parallel rows=100 cols=100000 threads=32
+make r-threadpool rows=100 cols=100000 threads=32
+make r-parallel rows=100 cols=100000 threads=48
+make r-threadpool rows=100 cols=100000 threads=48
+make r-parallel rows=100 cols=100000 threads=64
+make r-threadpool rows=100 cols=100000 threads=64
+
+make r-parallel rows=110 cols=100000 threads=32
+make r-threadpool rows=110 cols=100000 threads=32
+make r-parallel rows=110 cols=100000 threads=48
+make r-threadpool rows=110 cols=100000 threads=48
+make r-parallel rows=110 cols=100000 threads=64
+make r-threadpool rows=110 cols=100000 threads=64
+
+make r-parallel rows=120 cols=100000 threads=32
+make r-threadpool rows=120 cols=100000 threads=32
+make r-parallel rows=120 cols=100000 threads=48
+make r-threadpool rows=120 cols=100000 threads=48
+make r-parallel rows=120 cols=100000 threads=64
+make r-threadpool rows=120 cols=100000 threads=64
+
+make r-parallel rows=130 cols=100000 threads=32
+make r-threadpool rows=130 cols=100000 threads=32
+make r-parallel rows=130 cols=100000 threads=48
+make r-threadpool rows=130 cols=100000 threads=48
+make r-parallel rows=130 cols=100000 threads=64
+make r-threadpool rows=130 cols=100000 threads=64
+
+make r-parallel rows=140 cols=100000 threads=32
+make r-threadpool rows=140 cols=100000 threads=32
+make r-parallel rows=140 cols=100000 threads=48
+make r-threadpool rows=140 cols=100000 threads=48
+make r-parallel rows=140 cols=100000 threads=64
+make r-threadpool rows=140 cols=100000 threads=64
+
+make r-parallel rows=150 cols=100000 threads=32
+make r-threadpool rows=150 cols=100000 threads=32
+make r-parallel rows=150 cols=100000 threads=48
+make r-threadpool rows=150 cols=100000 threads=48
+make r-parallel rows=150 cols=100000 threads=64
+make r-threadpool rows=150 cols=100000 threads=64
